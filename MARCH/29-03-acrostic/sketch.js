a8790e9721daaf0d853282b3381b336f2eda8080
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-03-29 14:38:06 -0300

    acrostic

--- MARCH/29-03-acrostic/sketch.js
+++ MARCH/29-03-acrostic/sketch.js
@@ -1,5 +1,36 @@
+let API = 'edcd71e426b721150e90c02ced10a6948f32a9e20b930b14e';
+let url_0 = 'https://';
+let url_1 = 'api.wordnik.com/v4/words.json/search/';
+let url_2 = '?caseSensitive=false&limit=10&api_key=';
+
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	noCanvas();
+	let button = select('#submit');
+	let input = select('#input');
+
+	button.mousePressed(makeAcrostic);
+
+	function makeAcrostic() {
+		let word = input.value();
+		for (let i = 0; i < word.length; i++) {
+			let letter = word.charAt(i);
+			div = createDiv(letter).parent('#container');
+			pickWord(div, letter);
+		}
+	}
+
+	function pickWord(div, letter) {
+		let url = url_0 + url_1 + letter + url_2 + API;
+		loadJSON(url,gotData);
+
+		function gotData(data) {
+			let options = data.searchResults;
+			let selection = random(options);
+			div.html(selection.word);
+			console.log(selection.word);
+		}
+	}
 }
 
 function draw() {
