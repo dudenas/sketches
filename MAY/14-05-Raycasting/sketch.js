976590ed015c3da6b2183d3e0ebbb43828a301a5
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-05-14 17:27:04 +0300

    raycasting save

--- MAY/14-05-Raycasting/sketch.js
+++ MAY/14-05-Raycasting/sketch.js
@@ -1,8 +1,8 @@
 let clrs = [5, 250]
-let SW = 0.75
-let r = 4
-let nr = 1
-let totalFrames = 1200
+let SW = 1
+let r = 2
+let nr = 2
+let totalFrames = 600
 
 let walls = []
 let particle
@@ -10,27 +10,29 @@ let change = false
 
 function setup() {
 	createCanvas(540, 540)
+	noStroke()
 	fill(clrs[0], 255)
 	rect(0, 0, width, height)
+	let padd = -20
 	for (let i = 0; i < 5; i++) {
-		wall = new Boundary(random(width), random(height), random(width), random(height))
+		wall = new Boundary(random(-padd, width + padd), random(-padd, height + padd), random(-padd, width + padd), random(-padd, height + padd))
 		walls.push(wall)
 	}
-	walls.push(new Boundary(0, 0, width, 0))
-	walls.push(new Boundary(width, 0, width, height))
-	walls.push(new Boundary(width, height, 0, height))
-	walls.push(new Boundary(0, height, 0, 0))
+	walls.push(new Boundary(-padd, -padd, width + padd, -padd))
+	walls.push(new Boundary(width + padd, -padd, width + padd, height + padd))
+	walls.push(new Boundary(width + padd, height + padd, -padd, height + padd))
+	walls.push(new Boundary(-padd, height + padd, -padd, -padd))
 
 	particle = new Particle()
+	saveSetup()
 }
 
 function draw() {
-	if (frameCount % 240 == 0) change = !change
+	if (frameCount % (totalFrames / 4) == 0) change = !change
 	if (change) blendMode(SCREEN)
 	else blendMode(NORMAL)
-	// background(clrs[0])
-	fill(clrs[0], 15)
-	rect(0, 0, width, height)
+	fill(clrs[0], 25)
+	rect(-5, -5, width + 10, height + 10)
 
 	for (let wall of walls) {
 		wall.show()
@@ -42,7 +44,8 @@ function draw() {
 	let yoff = nr * sin(angle)
 	let noiseX = map(noise(xoff, yoff), 0, 1, 0, width)
 	let noiseY = map(noise(xoff + 100, yoff + 100), 0, 1, 0, height)
-	// SW = map(noise(noiseX / 25, noiseY / 25), 0, 1, 0.25, 0.75)
 	particle.update(noiseX, noiseY)
 	particle.show()
+
+	saveDraw()
 }
