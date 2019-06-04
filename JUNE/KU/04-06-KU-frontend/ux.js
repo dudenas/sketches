42d94ee927ccf786bede3b7e2e1382a447173e52
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-06-04 16:47:44 +0300

    ku chart added

---
+++ JUNE/KU/04-06-KU-frontend/ux.js
@@ -0,0 +1,190 @@
+let showColor = true
+let showSave = true
+let showMenu = false;
+let showData = false;
+
+// TOOLTIP
+
+// MODAL
+let mainGrfc = document.querySelector("#main")
+// GRAPH
+let modalGraph = document.querySelector("#modal-graph")
+// ABOUT
+let modalAbout = document.querySelector("#modal-about")
+// DATE
+let modalDate = document.querySelector("#modal-date")
+// BURGER MENU
+let modalBurgerOpen = document.querySelector("#modal-burger-open")
+let modalBurgerClose = document.querySelector("#modal-burger-close")
+// show hide
+modalBurgerClose.addEventListener('click', () => {
+  modalBurgerOpen.style.display = "block"
+  modalBurgerClose.style.display = "none"
+  $('.mobile-menu')
+    .removeClass('mobile-menu-show').addClass('mobile-menu-hidden')
+  $('.menu-outer-top')
+    .removeClass('mobile-menu-show').addClass('mobile-menu-hidden')
+})
+
+modalBurgerOpen.addEventListener('click', () => {
+  modalBurgerOpen.style.display = "none"
+  modalBurgerClose.style.display = "block"
+  $('.mobile-menu')
+    .removeClass('mobile-menu-hidden').addClass('mobile-menu-show')
+  $('.menu-outer-top')
+    .removeClass('mobile-menu-hidden').addClass('mobile-menu-show')
+})
+
+// hide data
+document.querySelector('#expand-mobile').addEventListener('click', () => {
+  let bot = document.querySelector(".menu-outer-bot");
+  let expBtn = document.querySelector("#expand-mobile");
+  if (!showData) {
+    let newVal = "var(--menu-bot-mobile) / 5"
+    bot.style.bottom = `calc(${newVal} * -4)`
+    expBtn.style.bottom = `calc(${newVal} + var(--main-diff))`
+  } else {
+    bot.style.bottom = "0px"
+    expBtn.style.bottom = "calc(var(--menu-bot-mobile) + var(--main-diff))"
+  }
+  showData = !showData
+})
+
+// close date
+// close date chosen
+// open graph
+document.querySelector("#graph").addEventListener('click', () => {
+  modalGraph.style.display = "block"
+  mainGrfc.style.filter = "blur(var(--blur-amount))"
+  chart.destroy();
+  chart = new Chart(ctx, config)
+});
+// close graph
+document.querySelector("#modal-graph-close").addEventListener('click', () => {
+  modalGraph.style.display = "none"
+  mainGrfc.style.filter = "blur(0px)"
+})
+
+// open about
+document.querySelector("#about").addEventListener('click', () => {
+  modalAbout.style.display = "block"
+  mainGrfc.style.filter = "blur(var(--blur-amount))"
+});
+// close about
+document.querySelector("#modal-about-close").addEventListener('click', () => {
+  modalAbout.style.display = "none"
+  mainGrfc.style.filter = "blur(0px)"
+})
+
+// open date
+document.querySelector("#date").addEventListener('click', () => {
+  modalDate.style.display = "block"
+  mainGrfc.style.filter = "blur(var(--blur-amount))"
+});
+// close date
+document.querySelector("#modal-date-close").addEventListener('click', () => {
+  modalDate.style.display = "none"
+  mainGrfc.style.filter = "blur(0px)"
+})
+// close date chosen
+document.querySelector("#modal-date-btn").addEventListener('click', () => {
+  modalDate.style.display = "none"
+  mainGrfc.style.filter = "blur(0px)"
+})
+
+
+// MENU
+// color switch
+function colorSwitch() {
+  let elements = document.querySelectorAll(".showColor");
+  elements.forEach(elm => {
+    if (!showColor) {
+      elm.style.display = "none"
+    } else {
+      elm.style.display = "block"
+    }
+  })
+
+  if (showColor) {
+    showSave = false
+    saveSwitch()
+  }
+  showColor = !showColor
+}
+
+// save switch
+function saveSwitch() {
+  let elements = document.querySelectorAll(".showSave");
+  elements.forEach(elm => {
+    if (!showSave) {
+      elm.style.display = "none"
+    } else {
+      elm.style.display = "block"
+    }
+  })
+
+  if (showSave) {
+    showColor = false
+    colorSwitch()
+  }
+  showSave = !showSave
+}
+
+// NAVIGATION
+// expand desktop
+function expandwitch() {
+  let top = document.querySelector(".menu-outer-top");
+  let bot = document.querySelector(".menu-outer-bot");
+  if (!showMenu) {
+    top.style.left = "calc(var(--menu-width) / 10 * 9 * -1)"
+    bot.style.left = "calc(var(--menu-width) / 10 * 9 * -1)"
+  } else {
+    top.style.left = "0px"
+    bot.style.left = "0px"
+  }
+
+  showMenu = !showMenu
+}
+
+// DATE
+// get time
+$(document).ready(function () {
+  $(".modal-content-date-time div").on("click", function () {
+    if (
+      $(this)
+      .next("ul")
+      .hasClass("closed")
+    ) {
+      $(".modal-content-date-time ul.opened")
+        .addClass('closed').removeClass('opened')
+        .hide();
+      $(this)
+        .next("ul")
+        .addClass('opened').removeClass('closed')
+        .show();
+      // rotate the icon
+      $(this)
+        .find("img")
+        .addClass('arrow-up').removeClass('arrow-down')
+    } else {
+      $(this)
+        .next("ul")
+        .addClass('closed').removeClass('opened')
+        .hide();
+      $(this)
+        .find("img")
+        .addClass('arrow-down').removeClass('arrow-up')
+    }
+  });
+
+  // update time
+  $(".modal-content-date-time ul li").on("click", function () {
+    $(this)
+      .parent("ul")
+      .addClass('closed').removeClass('opened')
+      .hide()
+      .parents(".time")
+      .find(".time-title")
+      .text($(this).text());
+  });
+});
