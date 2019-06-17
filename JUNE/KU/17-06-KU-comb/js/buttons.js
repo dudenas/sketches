6f4fd7cabb50c55a50def66ff9c655ad94a29504
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-06-17 18:25:05 +0300

    ku update

---
+++ JUNE/KU/17-06-KU-comb/js/buttons.js
@@ -0,0 +1,100 @@
+//—————————————————————————————————————————————————————— DATE
+// close date chosen
+document.querySelector("#modal-date-btn").addEventListener('click', () => {
+  modalDate.style.display = "none"
+  mainGrfc.style.filter = "blur(0px)"
+  // UPDATE PARAMETERS 
+  YEAR = tempYear
+  MONTH = tempMonth
+  DAY = tempDay
+
+  month = data[YEAR][MONTH]
+  day = month[DAY]
+  setFirstValues = true
+  setNewModalData = true
+
+  document.querySelectorAll(".data-date-target").forEach(elm => {
+    elm.innerHTML = `${YEAR} ${nf(MONTH,2,0)} ${nf(DAY,2,0)}`
+  })
+})
+
+//—————————————————————————————————————————————————————— DOCUMENT READY
+// functionality
+$(document).ready(function () {
+  // SAVE 
+  $('#saveDesktop').click(() => saveFunction(saveDesktop))
+  $('#saveMobile').click(() => saveFunction(saveMobile))
+  $('#saveInsta').click(() => saveFunction(saveInsta))
+  $('#saveFacebook').click(() => saveFunction(saveFacebook))
+  // PLAY 
+  $('#play').click(() => {
+    if (running) {
+      running = false
+      document.querySelector('.play-button').style.display = 'none'
+      document.querySelector('.stop-button').style.display = 'block'
+      console.log(running)
+    } else {
+      running = true
+      document.querySelector('.play-button').style.display = 'block'
+      document.querySelector('.stop-button').style.display = 'none'
+    }
+  })
+
+  // LANGUAGE
+  // set first language to lithuanian / remove en spans
+  // $("span:lang(en)").addClass('language-hide')
+
+  $('.language a').click(function (e) {
+    e.preventDefault()
+    if (this.className.split(/\s+/)[0] == 'lt') {
+      $('.en').addClass('language-passive').removeClass('language-active')
+      // document.documentElement.setAttribute('lang', 'lt')
+      // $("span:lang(lt)").addClass('language-show').removeClass('language-hide')
+      // $("span:lang(en)").addClass('language-hide').removeClass('language-show')
+    } else {
+      $('.lt').addClass('language-passive').removeClass('language-active')
+      // document.documentElement.setAttribute('lang', 'en')
+      // $("span:lang(en)").addClass('language-show').removeClass('language-hide')
+      // $("span:lang(lt)").addClass('language-hide').removeClass('language-show')
+    }
+    $(this).addClass('language-active').removeClass('language-passive')
+  })
+  // CLRS
+  $('.btn-clr').click(function (e) {
+    console.log(this.className)
+    e.preventDefault();
+    let myClass = this.className.split(/\s+/)
+    switch (myClass[1]) {
+      case 'grfc-black':
+        grfcBlack = true
+        grfcClr = grfcBlack ? clrs[3] : clrs[4]
+        background(grfcClr, 255)
+        break;
+      case 'grfc-white':
+        grfcBlack = false
+        grfcClr = grfcBlack ? clrs[3] : clrs[4]
+        background(grfcClr, 255)
+        break;
+    }
+
+    switch (myClass[2]) {
+      case 'bg-blue':
+        currClr = 1;
+        break;
+      case 'bg-red':
+        currClr = 2;
+        break;
+      case 'bg-yellow':
+        currClr = 0;
+        break;
+    }
+    // update background
+    bgClr = clrs[currClr]
+
+    // change higlighted
+    $(".btn-clr").addClass('btn-clr-passive').removeClass('btn-clr-active')
+    $(this).addClass('btn-clr-active').removeClass('btn-clr-passive')
+
+    return false;
+  });
+});
---
+++ JUNE/KU/17-06-KU-comb/js/share.js
@@ -0,0 +1,44 @@
+var twitterShare = document.querySelector('[data-js="twitter-share"]');
+
+twitterShare.onclick = function (e) {
+  e.preventDefault();
+  var twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');
+  if (twitterWindow.focus) {
+    twitterWindow.focus();
+  }
+  return false;
+}
+
+var facebookShare = document.querySelector('[data-js="facebook-share"]');
+
+// FB.init({
+//   appId: 453034275429574,
+//   status: true,
+//   cookies: true,
+//   xfbml: true
+// })
+facebookShare.onclick = function (e) {
+  e.preventDefault();
+  // FB.ui({
+  //   method: 'share',
+  //   name: 'This is the content of the "name" field.',
+  //   link: 'https://www.imdb.com/title/tt9428444/?ref_=nv_sr_1?ref_=nv_sr_1',
+  //   picture: 'https://m.media-amazon.com/images/M/MV5BMTc3MTkzYTgtZTYwMC00NjBiLWE0YzYtZjhkMjMyMDA5N2MyXkEyXkFqcGdeQXVyNjI2MzY3Mzg@._V1_UY268_CR149,0,182,268_AL_.jpg',
+  //   caption: 'Top 3 reasons why you should care about your finance',
+  //   description: "What happens when you don't take care of your finances? Just look at our country -- you spend irresponsibly, get in debt up to your eyeballs, and stress about how you're going to make ends meet. The difference is that you don't have a glut of taxpayers…",
+  //   message: ""
+  // });
+  // FB.ui({
+  //   method: 'share'
+  // }, function (response) {});
+  var facebookWindow = window.open(
+    // https://www.ku.lt/en/
+    // u=${document.URL}
+    // &quote=similique officia consectetur maxime placeat incidunt delectus corrupti repellendus repudiandae
+    // &description=this is a description
+    `https://www.facebook.com/sharer/sharer.php?`, 'facebook-popup', 'height=350,width=600');
+  if (facebookWindow.focus) {
+    facebookWindow.focus();
+  }
+  return false;
+}
