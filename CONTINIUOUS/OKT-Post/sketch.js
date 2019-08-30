cd8128d8bcb7222e2646705425d17c855b9bc649
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-08-30 16:59:50 +0300

    OKT Post

--- CONTINIUOUS/OKT-Post/sketch.js
+++ CONTINIUOUS/OKT-Post/sketch.js
@@ -1,5 +1,5 @@
 // Style
-const clrs = [250, 5, '#E51717']
+const clrs = [255, 0, '#E51717']
 let myFont;
 
 // text used
@@ -7,6 +7,12 @@ let word = 'OKT generator'
 
 // PGraphics
 let pg
+
+// top padding
+let topPadding = 0.8;
+
+// save
+let saveIt = false;
 
 function preload() {
 	// load font
@@ -23,25 +29,47 @@ function setup() {
 	textSize(txtSize)
 
 	pg = createGraphics(width, height);
-	pg.textSize(txtSize)
-	pg.textFont(myFont)
-	pg.textAlign(CENTER, TOP);
 }
 
 function draw() {
 	background(clrs[0])
 	const pushLine = txtSize / 5 * 3 + yMaster + yLines
-	drawPG(pushLine)
-	stroke(clrs[2])
-	noFill()
-	line(0, pushLine - copyLines, width, pushLine - copyLines)
-	line(0, pushLine + copyLines, width, pushLine + copyLines)
 	noStroke()
+
+	// draw copy ones
 	fill(clrs[1])
 	text(word, width / 2, yMaster)
-	const temp = createImage(width, copyLines * 2)
-	temp.copy(this, 0, pushLine - copyLines, width, copyLines * 2, 0, 0, width, temp.height)
-	image(temp, 0, height / 2);
+	drawLines(pushLine)
+	// on top graphics
+	fill(clrs[0])
+	rect(0, 0, width, txtSize * 5 / 4 * topPadding)
+	fill(clrs[1])
+	text(word, width / 2, yMaster)
+
+	// stroke
+	if (!saveIt) {
+		stroke(clrs[2])
+		noFill()
+		line(0, pushLine - copyLines, width, pushLine - copyLines)
+		line(0, pushLine + copyLines, width, pushLine + copyLines)
+	} else {
+		saveIt = false;
+		save(`OKT-${word}.png`)
+	}
 }
 
-function drawPG() {}
+// DRAW
+function drawLines(pushLine) {
+	const temp = createImage(width, int(copyLines * 2))
+	temp.copy(this, 0, pushLine - copyLines, width, copyLines * 2, 0, 0, width, temp.height)
+	const y = txtSize * 5 / 4 * topPadding
+	image(temp, 0, y);
+	repeatImage(temp, y, 1)
+}
+
+// REPEAT
+function repeatImage(temp, y, curr) {
+	image(temp, 0, y + yCopyLine * curr * gap);
+	curr++
+	if (curr < times) repeatImage(temp, y, curr)
+}
--- CONTINIUOUS/OKT-Post/ux.js
+++ CONTINIUOUS/OKT-Post/ux.js
@@ -6,7 +6,7 @@ $(inputTxt).change(function () {
 
 // SLIDERS
 const sliderSize = document.getElementById('slider_size')
-let txtSize = 48
+let txtSize = 120
 
 $(sliderSize).change(function () {
   $(this).prev().html(`size / ${this.value}`);
@@ -22,6 +22,8 @@ $(sliderSize).change(function () {
   copyLines = (tempValue * tempSlider.max) / tempMax;
   tempSlider.value = copyLines;
 
+  yCopyLine = copyLines * 2;
+
   const tempSliderY = $(sliderY)[0]
   const tempSliderYMax = tempSliderY.max
   const tempSliderYValue = tempSliderY.value
@@ -34,18 +36,60 @@ $(sliderSize).change(function () {
 
 let yMaster = 0;
 
+// COPY LINES
+let yCopyLine = 25; // define so they would be one next to each other
+// Y
 const sliderY = document.getElementById('slider_y')
 let yLines = 0;
-
 $(sliderY).change(function () {
-  $(this).prev().html(`y / ${this.value}`);
+  $(this).prev().html(`copy y / ${this.value}`);
   yLines = int(this.value);
 });
 
 
+// height
 const sliderCopyLines = document.getElementById('slider_copyLines')
-let copyLines = 48;
+let copyLines = 12;
 
 $(sliderCopyLines).change(function () {
   copyLines = int(this.value);
+  yCopyLine = copyLines * 2;
+});
+
+// TOP
+const sliderCopyTop = document.getElementById('slider_copyTop')
+$(sliderCopyTop).change(function () {
+  $(this).prev().html(`copy top / ${this.value / 100}`);
+  topPadding = float(int(this.value) / 100)
+});
+
+// gap
+const sliderCopyGap = document.getElementById('slider_copyGap')
+let gap = 1;
+$(sliderCopyGap).change(function () {
+  $(this).prev().html(`copy gap / ${this.value / 100}`);
+  gap = float(int(this.value) / 100)
+});
+
+// times
+const sliderCopyTimes = document.getElementById('slider_copyTimes')
+let times = 40;
+$(sliderCopyTimes).change(function () {
+  $(this).prev().html(`copy times / ${this.value}`);
+  times = int(this.value)
+});
+
+// TOGGLE
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      clrs[0] = 255
+      clrs[1] = 0
+    } else {
+      clrs[0] = 0
+      clrs[1] = 255
+    }
+  });
 });
